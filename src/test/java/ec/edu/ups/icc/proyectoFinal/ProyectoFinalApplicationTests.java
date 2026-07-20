70ed99767104b5704a399a1e86bab76373e44ad3
isaac-315 <isaacalf.135@gmail.com>
2026-07-20 09:53:32 -0500

    Primer commit: Proyecto Spring Boot inicial

---
+++ src/test/java/ec/edu/ups/icc/proyectoFinal/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.icc.proyectoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
